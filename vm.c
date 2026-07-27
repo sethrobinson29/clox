1666d5b5e988d73663eec6720b399e689a3fed48
Seth <sethpatrickrobinson@gmail.com>
2026-07-27 15:45:55 -0500

    Finishing inheritance

--- vm.c
+++ vm.c
@@ -398,6 +398,13 @@ static InterpretResult run() {
 				push(value);
 				break;
 			}
+			case OP_GET_SUPER: {
+				ObjString *name = READ_STRING();
+				ObjClass *superclass = AS_CLASS(pop());
+
+				if (!bindMethod(superclass, name)) { return INTERPRET_RUNTIME_ERROR; }
+				break;
+			}
 			case OP_EQUAL: {
 				Value b = pop();
 				Value a = pop();
@@ -481,6 +488,14 @@ static InterpretResult run() {
 				ObjString *method = READ_STRING();
 				int argCount = READ_BYTE();
 				if (!invoke(method, argCount)) { return INTERPRET_RUNTIME_ERROR; }
+				frame = &vm.frames[vm.frameCount - 1];
+				break;
+			}
+			case OP_SUPER_INVOKE: {
+				ObjString *method = READ_STRING();
+				int argCount = READ_BYTE();
+				ObjClass *superclass = AS_CLASS(pop());
+				if (!invokeFromClass(superclass, method, argCount)) { return INTERPRET_RUNTIME_ERROR; }
 				frame = &vm.frames[vm.frameCount - 1];
 				break;
 			}
